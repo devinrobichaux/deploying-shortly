9b1a1d8f5f4ae2cb1be7ab82e2b644d045e21bab
Devin <devinrobichaux@gmail.com>
2017-12-12 16:42:08 -0600

    basic functionality coming together... having issues with cookies

--- shortly.js
+++ shortly.js
@@ -1,51 +1,74 @@
-var express = require('express');
-var util = require('./lib/utility');
-var partials = require('express-partials');
-var bodyParser = require('body-parser');
+var express = require("express");
+var util = require("./lib/utility");
+var partials = require("express-partials");
+var bodyParser = require("body-parser");
+var session = require("express-session");
 
-
-var db = require('./app/config');
-var Users = require('./app/collections/users');
-var User = require('./app/models/user');
-var Links = require('./app/collections/links');
-var Link = require('./app/models/link');
-var Click = require('./app/models/click');
+var db = require("./app/config");
+var Users = require("./app/collections/users");
+var User = require("./app/models/user");
+var Links = require("./app/collections/links");
+var Link = require("./app/models/link");
+var Click = require("./app/models/click");
 
 var app = express();
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 app.use(partials());
 // Parse JSON (uniform resource locators)
 app.use(bodyParser.json());
 // Parse forms (signup/login)
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
+app.use(
+  session({
+    secret: "denied",
+    resave: false,
+    saveUninitialized: false,
+    // cookie: {},
+    user: undefined 
+  })
+);
 
-app.get('/', 
-function(req, res) {
-  res.render('index');
+function restrict(req, res, next) {
+  console.log(req.session.user);
+  if (req.session.user) {
+    // res.redirect('/')
+    next();
+  } else {
+    req.session.error = 'please login'
+    res.redirect("/login");
+  }
+}
+
+app.get("/", restrict, function(req, res) {
+  res.render("index");
 });
 
-app.get('/create', 
-function(req, res) {
-  res.render('index');
+app.get("/create", restrict, function(req, res) {
+  res.render("index");
 });
 
-app.get('/links', 
-function(req, res) {
-  Links.reset().fetch().then(function(links) {
-    res.status(200).send(links.models);
-  });
+app.get("/links", function(req, res) {
+  console.log(req.sessions);
+  // if (req.session.user) {
+    Links.reset()
+      .fetch()
+      .then(function(links) {
+        res.status(200).send(links.models);
+      });
+  // } else {
+  //   res.redirect("login");
+  // }
 });
 
-app.post('/links', 
-function(req, res) {
+app.post("/links", function(req, res) {
   var uri = req.body.url;
 
   if (!util.isValidUrl(uri)) {
-    console.log('Not a valid url: ', uri);
+    console.log("Not a valid url: ", uri);
     return res.sendStatus(404);
   }
 
@@ -55,7 +78,7 @@ function(req, res) {
     } else {
       util.getUrlTitle(uri, function(err, title) {
         if (err) {
-          console.log('Error reading URL heading: ', err);
+          // console.log("Error reading URL heading: ", err);
           return res.sendStatus(404);
         }
 
@@ -63,8 +86,7 @@ function(req, res) {
           url: uri,
           title: title,
           baseUrl: req.headers.origin
-        })
-        .then(function(newLink) {
+        }).then(function(newLink) {
           res.status(200).send(newLink);
         });
       });
@@ -76,39 +98,49 @@ function(req, res) {
 // Write your authentication routes here
 /************************************************************/
 
-var public = app.get('views')
+var public = app.get("views");
 
-
-app.get('/login', function(req, res) {
+app.get("/login", function(req, res) {
   // res.send('hello');
-  res.render(`${public}/login.ejs`)
+  res.render(`${public}/login.ejs`);
 });
 
-app.get('/signup', function(req, res) {
+app.get("/signup", function(req, res) {
   // res.send('hello');
-  res.render(`${public}/signup.ejs`)
+  res.render(`${public}/signup.ejs`);
 });
 
-app.post('/signup', 
-function(req, res) {
+app.post("/signup", function(req, res) {
   new User({
-    'username': req.body.username,
-    'password': req.body.password
-  }).save().then(function(newUser) {
-    res.redirect(200, '/');
-  });
+    username: req.body.username,
+    password: req.body.password
+  })
+    .save()
+    .then(function(newUser) {
+      req.session.user = true; 
+      // console.log(req.session.user);
+      // req.session.regenerate(function() {
+      //   req.session.user=req.body.username;
+        res.redirect(302, "/");
+        // console.log('this is signup', req.session.user);
+      });
+    // });
 });
 
-app.post('/login', 
-function(req, res) {
-  db.knex('users')
-    .where('username', '=', req.body.username)
-    .then(function(res) {
-
-      if (res[0]['password'] === req.body.password) {
-        res.render('index');
+app.post("/login", function(req, res) {
+  db
+    .knex("users")
+    .where("username", "=", req.body.username)
+    .then(function(data) {
+      if (data[0].password === req.body.password) {
+        // res.render('index');
+        // req.session.regenerate(function() {
+          req.session.user = true;
+          // console.log(req.session);
+          res.redirect(302, '/');
+          // console.log(req.session.user, 'this is login cookie');
       } else {
-        res.send('invalid password');
+        res.redirect(302, 'login');
       }
     });
 });
@@ -119,19 +151,19 @@ function(req, res) {
 // If the short-code doesn't exist, send the user to '/'
 /************************************************************/
 
-app.get('/*', function(req, res) {
+app.get("/*", function(req, res) {
   new Link({ code: req.params[0] }).fetch().then(function(link) {
     if (!link) {
-      res.redirect('/');
+      res.redirect("/");
     } else {
       var click = new Click({
-        linkId: link.get('id')
+        linkId: link.get("id")
       });
 
       click.save().then(function() {
-        link.set('visits', link.get('visits') + 1);
+        link.set("visits", link.get("visits") + 1);
         link.save().then(function() {
-          return res.redirect(link.get('url'));
+          return res.redirect(link.get("url"));
         });
       });
     }
